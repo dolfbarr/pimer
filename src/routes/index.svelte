df896a12f4cd1306b38664ed0b8625bb9ff6caa3
Dolf Barr <mail@dolf.me>
2022-07-22 11:58:47 +0200

    Add tailwind

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,7 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="container mx-auto min-h-screen w-max flex">
+  <div class="object-center self-center">
+    <h1 class="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+  </div>
+</div>
