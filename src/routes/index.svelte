579b60774bf066c1fe5200b841a740d180b82d2c
Dolf Barr <mail@dolf.me>
2022-07-24 19:42:43 +0200

    Add long breaks

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -7,6 +7,8 @@ import {slide, fade, draw } from 'svelte/transition';
 
   const WORK_TIMER = 'WORK_TIMER'
   const BREAK_TIMER = 'BREAK_TIMER'
+  const LONG_BREAK_TIMER = 'LONG_BREAK_TIMER'
+  const LONG_BREAK_APPEAR = 4
 
   const renderTime = (time) => time.toString().padStart(2, '0')
 
@@ -20,7 +22,8 @@ import {slide, fade, draw } from 'svelte/transition';
 
   const  timers = {
     [WORK_TIMER]: 0.1 * MINUTE_IN_MS,
-    [BREAK_TIMER]: 0.2 * MINUTE_IN_MS,
+    [BREAK_TIMER]: 0.1 * MINUTE_IN_MS,
+    [LONG_BREAK_TIMER]: 0.2 * MINUTE_IN_MS,
   }
   let session = []
   let isTimerGoing = false
@@ -30,8 +33,13 @@ import {slide, fade, draw } from 'svelte/transition';
 
   const pickNextTimer = () => {
     if (session.length && session[session.length - 1] === WORK_TIMER) {
-      currentTimer = tweened(timers[BREAK_TIMER])
-      currentTimerName = BREAK_TIMER
+      if ((session.length % (LONG_BREAK_APPEAR * 2 - 1)) === 0) {
+        currentTimer = tweened(timers[LONG_BREAK_TIMER])
+        currentTimerName = LONG_BREAK_TIMER
+      } else {
+        currentTimer = tweened(timers[BREAK_TIMER])
+        currentTimerName = BREAK_TIMER
+      }
     } else {
       currentTimer = tweened(timers[WORK_TIMER])
       currentTimerName = WORK_TIMER
@@ -44,10 +52,16 @@ import {slide, fade, draw } from 'svelte/transition';
     isTimerPaused = !isTimerPaused
   }
 
+  let timerInterval = -1
+
   const startTimer = () => {
     isTimerGoing = true
 
-    const timerInterval = setInterval(function () {
+    timerInterval = setInterval(function () {
+      if (!isTimerGoing) {
+        clearInterval(timerInterval)
+      }
+
       if (!isTimerPaused) {
         if ($currentTimer > 0) {
           $currentTimer -= 1000
@@ -67,12 +81,34 @@ import {slide, fade, draw } from 'svelte/transition';
       }
     }, SECOND_IN_MS)
   }
+
+  const nextSession = () => {
+    isTimerGoing = false
+    clearInterval(timerInterval)
+    session = [...session, currentTimerName]
+    pickNextTimer()
+  }
+
+  const prevSession = () => {
+    isTimerGoing = false
+    clearInterval(timerInterval)
+    session = [...session.slice(0, -1)]
+    pickNextTimer()
+  }
 </script>
 
-<div class="container mx-auto min-h-screen min-w-full flex justify-center transition-all {currentTimerName === BREAK_TIMER && 'bg-lime-500'} {currentTimerName === BREAK_TIMER && 'text-white'}">
+<div class="container relative mx-auto min-h-screen min-w-full flex justify-center transition-all {currentTimerName !== WORK_TIMER && 'bg-lime-500'} {currentTimerName !== WORK_TIMER && 'text-white'}">
+  <div class="absolute top-0 right-0 m-5">
+    <button class="self-center h-16 w-16 hover:rotate-180 transition-all">
+      <svg viewBox="0 0 24 24" fill="none" stroke-linecap="round" stroke-linejoin="round" class="feather feather-settings h-full w-full stroke-current">
+        <circle cx="12" cy="12" r="3"></circle>
+        <path d="M19.4 15a1.65 1.65 0 0 0 .33 1.82l.06.06a2 2 0 0 1 0 2.83 2 2 0 0 1-2.83 0l-.06-.06a1.65 1.65 0 0 0-1.82-.33 1.65 1.65 0 0 0-1 1.51V21a2 2 0 0 1-2 2 2 2 0 0 1-2-2v-.09A1.65 1.65 0 0 0 9 19.4a1.65 1.65 0 0 0-1.82.33l-.06.06a2 2 0 0 1-2.83 0 2 2 0 0 1 0-2.83l.06-.06a1.65 1.65 0 0 0 .33-1.82 1.65 1.65 0 0 0-1.51-1H3a2 2 0 0 1-2-2 2 2 0 0 1 2-2h.09A1.65 1.65 0 0 0 4.6 9a1.65 1.65 0 0 0-.33-1.82l-.06-.06a2 2 0 0 1 0-2.83 2 2 0 0 1 2.83 0l.06.06a1.65 1.65 0 0 0 1.82.33H9a1.65 1.65 0 0 0 1-1.51V3a2 2 0 0 1 2-2 2 2 0 0 1 2 2v.09a1.65 1.65 0 0 0 1 1.51 1.65 1.65 0 0 0 1.82-.33l.06-.06a2 2 0 0 1 2.83 0 2 2 0 0 1 0 2.83l-.06.06a1.65 1.65 0 0 0-.33 1.82V9a1.65 1.65 0 0 0 1.51 1H21a2 2 0 0 1 2 2 2 2 0 0 1-2 2h-.09a1.65 1.65 0 0 0-1.51 1z"></path>
+      </svg>
+    </button>
+  </div>
     <div class="object-center self-center flex flex-col justify-center align-middle">
       <div class="text-9xl font-sans flex">
-        <button class="self-center h-32 w-32">
+        <button class="self-center h-32 w-32 {!session.length && 'invisible'}" on:click={prevSession}>
           <svg viewBox="0 0 24 24" fill="none" stroke-linecap="round" stroke-linejoin="round" class="feather feather-chevron-left h-full w-full stroke-current">
             <polyline points="15 18 9 12 15 6"></polyline>
           </svg>
@@ -82,7 +118,7 @@ import {slide, fade, draw } from 'svelte/transition';
           <div class="{isTimerGoing && 'animate-pulse'} relative top-[-8px]">:</div>
           <div class="flex-1">{getRemainderSeconds($currentTimer)}</div>
         </div>
-        <button class="self-center flex h-32 w-32">
+        <button class="self-center flex h-32 w-32" on:click={nextSession}>
          <svg viewBox="0 0 24 24" fill="none" stroke-linecap="round" stroke-linejoin="round" class="feather feather-chevron-right h-full w-full stroke-current">
             <polyline points="9 18 15 12 9 6"></polyline>
           </svg>
