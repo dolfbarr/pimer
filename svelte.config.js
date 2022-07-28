98cca3f3446b22b83c285d9411a5d47ec7570e5e
Dolf Barr <mail@dolf.me>
2022-07-28 14:40:13 +0200

    Update svelte config for GH Pages

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,8 @@
 import adapter from '@sveltejs/adapter-static'
 import preprocess from 'svelte-preprocess'
 
+const dev = process.env.NODE_ENV === 'development';
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
@@ -10,10 +12,17 @@ const config = {
 	}),
 
 	kit: {
-		adapter: adapter(),
+		adapter: adapter({
+			pages: 'build',
+			assets: 'build',
+		}),
 		prerender: {
 			default: true,
 		},
+		paths: {
+			base: dev ? '' : '/pimer',
+		},
+		appDir: 'internal',
 	},
 }
 
