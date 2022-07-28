5d024376a40d769dbc9f0bfc2f4e8175e492e3d7
Dolf Barr <mail@dolf.me>
2022-07-28 14:22:47 +0200

    Update build to static render

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
-import adapter from '@sveltejs/adapter-auto';
-import preprocess from 'svelte-preprocess';
+import adapter from '@sveltejs/adapter-static'
+import preprocess from 'svelte-preprocess'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -10,8 +10,11 @@ const config = {
 	}),
 
 	kit: {
-		adapter: adapter()
-	}
-};
+		adapter: adapter(),
+		prerender: {
+			default: true,
+		},
+	},
+}
 
 export default config;
